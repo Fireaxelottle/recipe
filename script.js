50e6ef1287f46b43ec1dc237dee38c5a10c49be4
Fireaxelottle <157718129+Fireaxelottle@users.noreply.github.com>
2024-06-29 18:05:43 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 // variables
 let mealsElement = document.getElementById("meals"); // Get element with ID "meals"
 let favMealsContainer = document.getElementById("fav-meals"); // Get element with ID "fav-meals"
-
+const mealPopup = document.getElementById('meal-popup');
+const closePopup = document.getElementById('close-popup')
+const Recpie = document.querySelector('#meal-info h1')
 // Brain
 
 // Get random meal and add it to the screen
-GetRandomMeal();
+for (let i = 0; i < 5; i++) {
+  GetRandomMeal();
+}
+
 
 // Get favorite meals from local storage and add them to the screen
 getFavMeals();
@@ -35,31 +40,52 @@ async function GetMealById(id) {
 }
 
 // Fetch meals by search term from API
-async function GetMealBySearch() {
-  let response = await fetch(
-    "https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata"
+async function GetMealBySearch(mealname) {
+  let resp = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/search.php?s=" + mealname
   );
-  const Meals = await response.json();
+  const respData = await resp.json();
+  const SearchMeal = respData.meals[0];
+
+  addMeal(SearchMeal);
+
+
 }
+
+
 
 // Add meal to the screen
 function addMeal(mealData, random = false) {
+  const mealContainer = document.getElementById('meals');
   const meal = document.createElement("div");
   meal.classList.add("meal");
 
   meal.innerHTML = `  <div class="meal-header">
-              ${random && `<span class="random"> Random recipie </span>`}
-                    <img id="meal-img"  src="${mealData.strMealThumb}"
+             
+                <img id="meal-img"  src="${mealData.strMealThumb}"
                         alt="${mealData.strMeal}" />
                 </div>
                 <div class="meal-body">
                     <h4>${mealData.strMeal}</h4>
+                    <button class="more-btn" id="more-btn">
+                        Show More
+                    </button>
                     <button class="fav-btn" id="fav-btn">
                         <i class="fa-regular fa-heart" id="fav-btn-icon"></i>
                     </button>
+
                 </div>  `;
+
+  // some variables
   const favBtn = meal.querySelector(".fav-btn");
   const favBtnIcon = meal.querySelector("#fav-btn-icon");
+  const moreBtn = meal.querySelector(".more-btn");
+
+  // show recpie button
+  moreBtn.addEventListener("click", () => {
+    mealPopup.classList.remove('hidden');
+    Recpie.innerHTML = mealData.strInstructions;
+  });
 
   // Add event listener to favorite button
   favBtn.addEventListener("click", () => {
@@ -78,7 +104,11 @@ function addMeal(mealData, random = false) {
     }
 
     getFavMeals(); // Get favorite meals and add them to the screen
+
+    // show recpie
+
   });
+
 
 
   mealsElement.appendChild(meal);
@@ -114,15 +144,15 @@ async function getFavMeals() {
 
   mealIDs.forEach(async (mealID) => {
     const mealId = mealID;
-    
+
     meal = await GetMealById(mealId);
     addFavMeal(meal);
-    
+
   });
 }
 
 // Add favorite meal to the screen
-async function addFavMeal(mealData){
+async function addFavMeal(mealData) {
 
   const favMeal = document.createElement("li");
 
@@ -135,16 +165,44 @@ async function addFavMeal(mealData){
   `;
 
   const btn = favMeal.querySelector(".clear");
+  const imgbtn = favMeal.querySelector('img');
 
+  imgbtn.addEventListener('click', () => {
+    mealPopup.classList.remove('hidden');
+    Recpie.innerHTML = mealData.strInstructions;
+
+  })
   // Add event listener to clear button
   btn.addEventListener("click", () => {
-      removeMeal_LocalStorage(mealData.idMeal); // Remove meal from local storage
+    removeMeal_LocalStorage(mealData.idMeal); // Remove meal from local storage
 
-      getFavMeals(); // Get favorite meals and add them to the screen
+    getFavMeals(); // Get favorite meals and add them to the screen
+
+
   });
 
 
   favMealsContainer.appendChild(favMeal);
 
-}
+};
 
+
+// getmealbysearch
+
+const input = document.getElementById('search-term');
+const form = document.getElementById('form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  GetMealBySearch(input.value);
+
+
+})
+
+
+// closePopup button function
+
+closePopup.addEventListener('click', () => {
+  mealPopup.classList.add('hidden');
+  Recpie.innerHTML = "";
+});
